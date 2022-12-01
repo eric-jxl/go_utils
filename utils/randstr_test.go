846fe2976be5cac806fac947bf449ed65fdb15a6
eric <799050481@qq.com>
2022-12-01 15:57:43 +0800

    add testfile
    Signed-off-by: eric <799050481@qq.com>

---
+++ utils/randstr_test.go
@@ -0,0 +1,25 @@
+package utils
+
+import "testing"
+
+func Test_randomString(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"test",  args{n: 10}, "rfBd67ti3S,"}, {"test2", args{36}, "MtYvSgD6xAV1YU00zampta8Z8S686KLkIZ0P,"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := randomString(tt.args.n); got != tt.want {
+				t.Logf("randomString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
