82fb2b0ea3d4b5961aae1d99c5b21b935c3c7347
eric <799050481@qq.com>
2022-12-06 20:08:03 +0800

    modify
    Signed-off-by: eric <799050481@qq.com>

--- utils/randstr_test.go
+++ utils/randstr_test.go
@@ -12,13 +12,14 @@ func Test_randomString(t *testing.T) {
 		want string
 	}{
 		// TODO: Add test cases.
-		{"test",  args{n: 10}, "rfBd67ti3S,"}, {"test2", args{36}, "MtYvSgD6xAV1YU00zampta8Z8S686KLkIZ0P,"},
+		{"test", args{10}, "rfBd67ti3S,"},
+		{"test2", args{36}, "MtYvSgD6xAV1YU00zampta8Z8S686KLkIZ0P,"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := randomString(tt.args.n); got != tt.want {
-				t.Logf("randomString() = %v, want %v", got, tt.want)
+				t.Fatalf("randomString() = %v, want %v", got, tt.want)
 			}
 		})
 	}
